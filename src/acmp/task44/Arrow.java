d0205eaa4057d258f5bfc67901e72bdf6f9ea671
Adyl <Adyl>
2017-11-15 23:43:27 +0600

    task 44 Arrow

---
+++ src/acmp/task44/Arrow.java
@@ -0,0 +1,21 @@
+package acmp.task44;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Arrow {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String srting = reader.readLine();
+        char[] chars = srting.toCharArray();
+        int d = 0;
+        for (int i = 0; i < chars.length-4; i++) {
+            if ((chars[i] == '>' && chars[i + 1] == '>' && chars[i + 2] == '-' && chars[i + 3] == '-' && chars[i + 4] == '>')||(chars[i] == '<' && chars[i+1] == '-' && chars[i+2] == '-' && chars[i+3] == '<' && chars[i+4] == '<')) {
+                d++;
+            }
+        }
+        System.out.println(d);
+    }
+}
