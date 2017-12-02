e52ec90923158439127560605705f4aca846b94e
mirlan <mt.uulu@gmail.com>
2017-12-02 13:52:31 -0500

    new tasks

--- src/acmp/data_types/DataTypes.java
+++ src/acmp/data_types/DataTypes.java
@@ -1,24 +1,23 @@
-package acmp.problemHack;
+package acmp.data_types;
 
 import acmp.utils.AdylUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Objects;
 
-public class Datelines {
+public class DataTypes {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int num = AdylUtils.readInt();
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < num; i++) {
             String s = reader.readLine();
             try {
-                result += getBits(s);
+                result.append(getBits(s));
             } catch (Exception e) {
-                result +="\n"+ s + " can't be fitted anywhere.";
+                result.append("\n").append(s).append(" can't be fitted anywhere.");
             }
         }
         System.out.println(result);
@@ -37,9 +36,7 @@ public class Datelines {
             string += "\n* int ; ";
         }
 
-        if ((l >= -9223372036854775808L) && (l <= 9223372036854775807L)) {
-            string += "\n* Long ; ";
-        }
+        string += "\n* Long ; ";
 
         return string;
     }
