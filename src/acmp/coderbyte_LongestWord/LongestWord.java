c214c7bda079c6e83d3c79965c67c24a8221f91f
Adyl <Adyl>
2017-12-18 22:35:58 +0600

    new soluton 'coderbyte'

--- src/acmp/coderbyte_LongestWord/LongestWord.java
+++ src/acmp/coderbyte_LongestWord/LongestWord.java
@@ -7,16 +7,21 @@ import java.io.IOException;
 public class LongestWord {
     public static void main(String[] args) throws IOException {
         String string = AdylUtils.readString();
-        String[] sen = string.split(" ");
-        String result = longestWord(sen);
+        char[] sen = string.toLowerCase().toCharArray();
+        String s = "";
+        for (char i : sen) {
+            s += (i >= 97 && i <= 122) ? i : ' ';
+        }
+        String[] stringChar = s.split(" ");
+        String result = longestWord(stringChar);
         System.out.println(result);
 
     }
 
-    private static String longestWord(String[] sen) {
+    private static String longestWord(String[] stringChar) {
         String string = "";
-        for (String i : sen){
-            if (i.length() > string.length() ){
+        for (String i : stringChar) {
+            if (i.length() > string.length()) {
                 string = i;
             }
         }
